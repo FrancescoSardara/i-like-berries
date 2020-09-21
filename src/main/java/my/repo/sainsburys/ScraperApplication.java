03af51eaf6ec3908802c8ee5e214c4974da28b51
Francesco Sardara <francescosardara@rentalcars.com>
2020-09-21 10:25:52 +0100

    first commit: setup application with gradle and libraries of choice

---
+++ src/main/java/my/repo/sainsburys/ScraperApplication.java
@@ -0,0 +1,21 @@
+package my.repo.sainsburys;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Log4j2
+@SpringBootApplication
+public class ScraperApplication implements CommandLineRunner {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScraperApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Start scraping url: {}", "Saintsbutys");
+    }
+}
---
+++ src/test/java/my/repo/sainsburys/ScraperApplicationTest.java
@@ -0,0 +1,13 @@
+package my.repo.sainsburys;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScraperApplicationTest {
+
+    @Test
+    void name() {
+        assertTrue(true);
+    }
+}
